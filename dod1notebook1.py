d29e9f05417338588e60c93fabe508ddc12f5c82
nm-nithies <155542254+nm-nithies@users.noreply.github.com>
2024-10-07 11:38:41 +0530

    Update dod1notebook1.py

--- dod1notebook1.py
+++ dod1notebook1.py
@@ -60,4 +60,21 @@ In file included from /usr/include/c++/9/bits/stl_algobase.h:67,
 /usr/include/c++/9/bits/stl_iterator.h:423:5: note:   template argument deduction/substitution failed:
 ../src/Dialect/ONNX/Transforms/temp.cpp:718:63: note:   'mlir::FloatAttr' is not derived from 'const std::reverse_iterator<_Iterator>'
   718 |     FloatAttr momentumAttr =  rewriter.getF32FloatAttr(0.9) + a;
-      |                                                               ^
+      |                                                      
+
+
+auto a = mlir::cast<FloatAttr>(subConstValue);
+
+
+/usr/bin/c++ -DONNX_ML=1 -DONNX_MLIR_DECOMP_ONNX_CONVTRANSPOSE -DONNX_MLIR_ENABLE_STABLEHLO -DONNX_NAMESPACE=onnx -D_DEBUG -D_GLIBCXX_ASSERTIONS -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -I/workspace/ONNX_MLIR/llvm-project/llvm/include -I/workspace/ONNX_MLIR/llvm-project/build/include -I/workspace/ONNX_MLIR/llvm-project/mlir/include -I/workspace/ONNX_MLIR/llvm-project/build/tools/mlir/include -I../ -I. -I../include -I../third_party/onnx -Ithird_party/onnx -I_deps/protobuf-src/src -I_deps/abseil-src -fPIC -fno-semantic-interposition -fvisibility-inlines-hidden -Werror=date-time -fno-lifetime-dse -Wall -Wextra -Wno-unused-parameter -Wwrite-strings -Wcast-qual -Wno-missing-field-initializers -Wimplicit-fallthrough -Wno-nonnull -Wno-class-memaccess -Wno-redundant-move -Wno-pessimizing-move -Wno-noexcept-type -Wdelete-non-virtual-dtor -Wsuggest-override -Wno-comment -Wno-misleading-indentation -fdiagnostics-color -ffunction-sections -fdata-sections -DSUPPRESS_THIRD_PARTY_WARNINGS -O3 -DNDEBUG   -D_DEBUG -D_GLIBCXX_ASSERTIONS -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS -UNDEBUG -pthread -std=gnu++17 -MD -MT src/Dialect/ONNX/Transforms/CMakeFiles/OMONNXRewrite.dir/temp.cpp.o -MF src/Dialect/ONNX/Transforms/CMakeFiles/OMONNXRewrite.dir/temp.cpp.o.d -o src/Dialect/ONNX/Transforms/CMakeFiles/OMONNXRewrite.dir/temp.cpp.o -c ../src/Dialect/ONNX/Transforms/temp.cpp
+In file included from /workspace/ONNX_MLIR/llvm-project/mlir/include/mlir/IR/AffineMap.h:18,
+                 from /workspace/ONNX_MLIR/llvm-project/mlir/include/mlir/IR/BuiltinAttributeInterfaces.h:12,
+                 from /workspace/ONNX_MLIR/llvm-project/mlir/include/mlir/IR/BuiltinAttributes.h:12,
+                 from /workspace/ONNX_MLIR/llvm-project/mlir/include/mlir/IR/Matchers.h:18,
+                 from ../src/Dialect/ONNX/Transforms/temp.cpp:23:
+/workspace/ONNX_MLIR/llvm-project/mlir/include/mlir/IR/Value.h: In instantiation of 'static To llvm::CastInfo<To, From, typename std::enable_if<(is_same_v<mlir::Value, typename std::remove_const<From>::type> || is_base_of_v<mlir::Value, From>), void>::type>::doCast(mlir::Value) [with To = mlir::FloatAttr; From = mlir::Value]':
+/workspace/ONNX_MLIR/llvm-project/llvm/include/llvm/Support/Casting.h:573:36:   required from 'decltype(auto) llvm::cast(From&) [with To = mlir::FloatAttr; From = mlir::Value]'
+../src/Dialect/ONNX/Transforms/temp.cpp:703:49:   required from here
+/workspace/ONNX_MLIR/llvm-project/mlir/include/mlir/IR/Value.h:619:55: error: no matching function for call to 'mlir::FloatAttr::FloatAttr(mlir::detail::ValueImpl*)'
+  619 |   static inline To doCast(mlir::Value value) { return To(value.getImpl()); }
+      |                                                       ^~~~~~~~~~~~~~~~~~~
